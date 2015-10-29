dadcb8c8b093fdeae390ce5ac026d3fd6436cb76
Doug Read <dreadjr@gmail.com>
2015-10-29 12:31:29 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,108 @@
+var AWS = require('aws-sdk');
+var Promise = require('bluebird');
+var Firebase = require('firebase');
+var debug = require('debug');
+var error = debug('firebase-to-sqs:error');
+var log = debug('firebase-to-sqs:error');
+
+
+function submitToSqsBatch(params) {
+  //var params = {
+  //  Entries: _.map(jobs, function(job, idx) {
+  //    return {
+  //      Id: job.msg.search._id,
+  //      MessageBody: JSON.stringify(job.msg)
+  //    }
+  //  }),
+  //
+  //  QueueUrl: queueUrl(job.queue)
+  //};
+
+  var self = this;
+
+  return new Promise(function(resolve, reject) {
+    self.sqs.sendMessageBatch(params, function (err, data) {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(data);
+    });
+  });
+}
+
+function submitToSqs(job) {
+  var params = {
+    MessageBody: JSON.stringify(job.msg),
+    QueueUrl: queueUrl(job.queue)
+  };
+
+  return new Promise(function(resolve, reject) {
+    sqs.sendMessage(params, function (err, data) {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(data);
+    });
+  });
+}
+
+function mapToSqsBatch(records) {
+  var max = 10;
+
+  function queueUrl(queue) {
+    return process.env.AWS_QUEUE_PREFIX + queue;
+  }
+
+  var chain = _.chain(records)
+    .groupBy('queue')
+    .map(function(v, k) {
+      return _.chunk(v, max).map(function (c) {
+        return {
+          QueueUrl: queueUrl(k),
+          Entries: _.map(c, function (job, idx) {
+            return {
+              Id: job.msg.search._id,
+              MessageBody: JSON.stringify(job.msg)
+            };
+          })
+        };
+      });
+    })
+    .flatten();
+
+  return Promise.resolve(chain.value());
+}
+
+function factory(func) {
+  var sqs = new AWS.SQS();
+  var fb = new Firebase(process.env.FIREBASE_URL);
+  var fbAuth = Promise.promisify(fb.authWithCustomToken, fb);
+
+  return function(event, context) {
+    fbAuth(process.env.FIREBASE_SECRET)
+      .then(function(authData) {
+        return func.call({ firebase: fb}, event);
+      })
+      .then(mapToSqsBatch)
+      .bind({ sqs: sqs})
+      .map(submitToSqsBatch)
+
+      .catch(function(err) {
+        error(err, err.stack);
+        context.fail(error);
+      })
+      .then(function() {
+        context.done();
+      });
+  };
+}
+
+module.exports = {
+  factory: factory,
+
+  submitToSqsBatch: submitToSqsBatch,
+  submitToSqs: submitToSqs,
+  mapToSqsBatch: mapToSqsBatch
+};
